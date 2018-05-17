04afa8421912f8d5cec9b13508aef70e819e0a85
Dijup Tuladhar <dezudas@gmail.com>
2018-05-17 11:00:59 +0545

    Package Name Update

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-	name: 'milligram:milligram-scss',
-	version: '1.3.0',
+	name: 'gaida',
+	version: '1.0.0',
 	summary: 'A minimalist CSS framework on SCSS version.',
-	git: 'https://github.com/milligram/milligram-scss.git',
+	git: 'https://github.com/dezudas/gaida.git',
 	documentation: 'readme.md'
 });
 
 Package.onUse(function (api) {
 	api.versionsFrom('METEOR@1.0');
 	api.addFiles([
-		'dist/milligram.scss'
+		'dist/gaida.scss'
 	], 'client');
 });
